6ff7cc1417108a1ee1a0500b391afbdca73a83f1
Kim Ahlström <kim.ahlstrom@gmail.com>
2011-02-28 16:23:21 +0900

    Tabs to spaces, grrr. And add help text

--- app/tack.pm
+++ app/tack.pm
@@ -12,21 +12,45 @@ BEGIN {
 }
 
 use App::Tack::Template;
+use constant REPLACE => '<TACK>';
 
 our @templates = (
-	App::Tack::Template->new(qr{map}, qr{\b (def|class|module|private|protected) \b}),
-	App::Tack::Template->new(qr{sub}, qr{sub \s+ <TACK>}),
+  App::Tack::Template->new(qr{map}, qr{\b (def|class|module|private|protected) \b}),
+  App::Tack::Template->new(qr{sub}, qr{sub \s+ <TACK>}),
 );
 
 sub run {
-	my $command = shift;
-	my @options = @_;
-	
-	foreach my $template ( @templates ) {
-		if ( $command =~ $template->name ) {
-			print "hej\n";
-		}
-	}
+  my $command = shift;
+  my @options = @_;
+  
+  if ( $command =~ /h (e (l (p)?)?)?/ix) {
+    print_help();
+    exit;
+  }
+  
+  foreach my $template ( @templates ) {
+    if ( $command =~ $template->name ) {
+      # Check for interpolations
+      my $interpolations = 0;
+      if ( $template->pattern =~ REPLACE ) {
+        
+      }
+      
+      # Remaining @options are flags to ack and must come first
+      
+      # Execute
+      system("ack");
+    }
+  }
+}
+
+sub print_help {
+  print <<END;
+usage: - tack template_name [template interpolations] [ack options]
+         Run ack with the template
+       - tack h|he|hel|help
+         Show this help
+END
 }
 
 1; # End of App::Tack
