b1a7456918cb717e7469dd454de5c57dd55fa6ef
Kim Ahlström <kim.ahlstrom@gmail.com>
2011-02-28 14:03:22 +0900

    Initial commit

---
+++ tack.pl
@@ -0,0 +1,16 @@
+package App::Tack;
+
+use warnings;
+use strict;
+
+our $VERSION;
+our $COPYRIGHT;
+BEGIN {
+    $VERSION = '0.1';
+    $COPYRIGHT = 'Copyright 2011 Kim Ahlström.';
+}
+
+1;
+
+use App::Tack;
+
