179ce628bdc24358f0c63416e481d9b358f40d56
Kim Ahlström <kim.ahlstrom@gmail.com>
2011-02-28 15:55:24 +0900

    Start working on the basics

--- tack.pl
+++ tack.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/env perl
+
 package App::Tack;
 
 use warnings;
@@ -10,7 +12,43 @@ BEGIN {
     $COPYRIGHT = 'Copyright 2011 Kim Ahlström.';
 }
 
-1;
+our @names = (
+	App::Tack::Template->new('map', /\b (def|class|module|private|protected) \b/ix),
+	App::Tack::Template->new('sub', /sub \s+ ___/ix),
+);
 
-use App::Tack;
+sub run {
+	my $command = shift;
+	my @options = @_;
+	
+	foreach ( my $template, @names ) {
+		if ( $command eq $template->name ) {
+			
+		}
+	}
+}
 
+1; # End of App::Tack
+
+package App::Tack::Template;
+
+use warnings;
+use strict;
+
+sub new {
+	my $class   = shift;
+	my $name    = shift;
+	my $pattern = shift;
+	
+	my $self = bless {
+		name => $name,
+		pattern => $pattern,
+	}, $class;
+	
+	return $self;
+}
+
+sub name { return shift->{name}; }
+sub pattern { return shift->{pattern}; }
+
+1; # End of App::Tack::Template
