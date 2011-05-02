0639c8e20a68e8aacfe915fd87a4789798a2af41
Kim Ahlström <kim.ahlstrom@gmail.com>
2011-05-02 12:26:05 +0900

    Make usage much more easy to understand

--- lib/App/Tack.pm
+++ lib/App/Tack.pm
@@ -15,7 +15,7 @@ use App::Tack::Template;
 
 # TODO: How to insert a constant into a regex?
 # TODO: Handle flags for patterns
-# TODO: Better handling of optional <TACK> interpolations
+# TODO: Better handling of optional <ARG> interpolations
 our @templates = (
   App::Tack::Template->new('map', '\b(def|class|module|private|protected)\b', 'Map Ruby files'),
   App::Tack::Template->new('name', sub {
@@ -25,8 +25,8 @@ our @templates = (
     $name =~ s/_/_\?/;
     
     return(["\\b($name)\\b", '-i']);
-  }, 'Map Ruby files'),
-  App::Tack::Template->new('(sub|def|func(t(i(o(n)?)?)?)?)', '\b(sub|def|function)(?:\s*<TACK>)?\b', 'Find subs/defs/functions, with optional name'),
+  }, 'Find the named symbol. Takes one required argument'),
+  App::Tack::Template->new('(sub|def|func(t(i(o(n)?)?)?)?)', '\b(sub|def|function)(?:\s*<ARG>)?\b', 'Find subs/defs/functions. Takes one optional argument'),
 );
 
 sub run {
@@ -34,12 +34,18 @@ sub run {
   my @options = @_;
   
   # TODO: Use Getopts::Long
-  if ( $command eq '' || $command =~ /--? h (e (l (p)?)?)?/ix) {
+  if ( $command eq '' ) {
     print_help();
+    print "\n";
+    print_templates();
     exit;
   }
   elsif ( $command =~ /--templates/ix ) {
     print_templates();
+    exit;
+  }
+  elsif ( $command =~ /--? h (e (l (p)?)?)?/ix) {
+    print_help();
     exit;
   }
   
@@ -48,9 +54,9 @@ sub run {
       my($pattern, $ack_opts) = @{&{$template->pattern}(\@options)};
       
       # Check for interpolations
-      while ( $pattern =~ m/<TACK>/ && scalar(@options) > 0 ) {
+      while ( $pattern =~ m/<ARG>/ && scalar(@options) > 0 ) {
         my $replacement = shift @options;
-        $pattern =~ s/<TACK>/$replacement/;
+        $pattern =~ s/<ARG>/$replacement/;
       }
       
       # Remaining @options are flags to ack and must come first
@@ -67,18 +73,23 @@ sub run {
 
 sub print_help {
   print <<END;
-usage: - tack template_name [template interpolations] [ack files and options]
-         Run ack with the template
-       - tack --h|he|hel|help
-         Show this help
-       - tack --templates
-         Print available templates
+== Usage
+
+- tack template_name [template interpolations] [ack files and options]
+  Run ack with the named template
+- tack --h|he|hel|help
+  Show this help
+- tack --templates
+  Print available templates
 END
 }
 
 sub print_templates {
+  print "== Available templates\n\n";
   foreach my $template ( @templates ) {
-    print '- ' . $template->name . ' - ' . $template->comment .  "\n  " . $template->pattern . "\n";
+    print '-> ' . $template->comment . "\n";
+    print '   Name: ' . $template->name . "\n";
+    print '   Pattern: ' . @{&{$template->pattern}(['ARG', 'ARG', 'ARG', 'ARG'])}[0] . "\n\n";
   }
 }
 
